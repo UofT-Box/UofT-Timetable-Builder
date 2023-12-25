b04bf69a56fc1c23b18cee4be44a679c92cae623
AugustZ <zhengzhongze4@gmail.com>
2023-12-24 23:31:26 -0500

    add walking distense

---
+++ scrap/walking_distense_main.py
@@ -0,0 +1,248 @@
+building_list = {
+    "WW": "Woodsworth College",
+    "OI": "Ontario Institute for Studies in Education",
+    "ES": "Earth Sciences Centre",
+    "GB": "Galbraith Building",
+    "BA": "Bahen Centre for Information Technology",
+    "EM": "Emmanuel College",
+    "AB": "Astronomy & Astrophysics Building",
+    "SS": "Sidney Smith Hall",
+    "KP": "Koffler House",
+    "NL": "C. David Naylor Building",
+    "WB": "Wallberg Building",
+    "SU": "Student Commons",
+    "AH": "Muzzo Family Alumni Hall",
+    "UC": "University College",
+    "PB": "Leslie L. Dan Pharmacy Building",
+    "HS": "Health Sciences Building",
+    "WI": "Wilson Hall",
+    "WE": "Wetmore Hall",
+    "FE": "720 Spadina Avenue",
+    "TF": "Teefy Hall",
+    "AP": "Anthropology Building",
+    "NF": "Northrop Frye Hall",
+    "MS": "Medical Sciences Building",
+    "CH": "Convocation Hall",
+    "BF": "Bancroft Building",
+    "BL": "Claude T. Bissell Building",
+    "VC": "Victoria College",
+    "RL": "Robarts Library",
+    "DA": "1 Spadina Crescent (Daniels Building)",
+    "BT": "Isabel Bader Theatre",
+    "CR": "Carr Hall",
+    "TC": "Trinity College",
+    "ML": "39 Queen's Park Cres East (Sir W.T. White House)",
+    "BR": "Brennan Hall",
+    "CG": "Canadiana Gallery",
+    "BC": "Birge-Carnegie Library",
+    "RG": "Regis College",
+    "LM": "Lash Miller Chemical Laboratories",
+    "LA": "Gerald Larkin Building",
+    "IN": "Innis College",
+    "JH": "Jackman Humanities Building",
+    "UY": "700 University Avenue",
+    "LI": "Lillian Massey Building",
+    "SD": "Sir Daniel Wilson Residence",
+    "UP": "University College Union",
+    "GM": "Luella Massey Studio Theatre",
+    "WO": "Woodsworth College Residence",
+    "EJ": "Edward Johnson Building",
+    "OH": "Odette (Louis) Hall",
+    "OA": "263 McCaul Street",
+    "SB": "Borden Building South",
+    "NB": "Borden Building North",
+    "RU": "Rehabilitation Sciences Building",
+    "JP": "90 Wellesley Street West",
+    "BN": "Clara Benson Building",
+    "WS": "Warren Stevens Building",
+    "RS": "Rosebrugh Building",
+    "RT": "Rotman School of Management",
+    "SM": "Gerstein Science Information Centre",
+    "MY": "Myhal Centre for Engineering Innovation & Entrepreneurship",
+    "HA":'Haultain Building',
+    "MB":'Lassonde Mining Building',
+    "MC":'Mechanical Engineering Building',
+    "SF":'Sandford Fleming Building',
+    "RW":'Ramsay Wright Laboratories',
+    "MP":'McLennan Physical Laboratories',
+    "SK":'246 Bloor Street West',
+    "HI":'St. Hilda\'s College',
+    "PR":'71 Queen\'s Park Crescent East Toronto',
+}
+address = '5 King\'s College Rd, Toronto, ON M5S 3G8'
+
+locations = {
+  "WW": [43.6663161, -79.3990297],
+  "OI": [43.667979, -79.3983677],
+  "ES": [43.6609659, -79.3997854],
+  "GB": [43.6598852, -79.39635249999999],
+  "BA": [43.65980450000001, -79.39729799999999],
+  "EM": [43.6668725, -79.3927925],
+  "AB": [43.6606173, -79.3973224],
+  "SS": [43.662523, -79.39854249999999],
+  "KP": [43.66049810000001, -79.40078079999999],
+  "NL": [43.660469, -79.3918499],
+  "WB": [43.6590757, -79.3951865],
+  "SU": [43.6587516, -79.3979178],
+  "AH": [43.6649125, -79.3901743],
+  "UC": [43.6631493,-79.39578329999999],
+  "PB": [43.65993290000001, -79.3916642],
+  "HS": [43.65908839999999, -79.39278399999999],
+  "WI": [43.6615473, -79.4008788],
+  "WE": [43.6622882, -79.4005188],
+  "FE": [43.6654103, -79.403571],
+  "TF": [43.6656331, -79.39083529999999],
+  "AP": [43.65991169999999,-79.39842209999999],
+  "NF": [43.666422, -79.39218609999999],
+  "MS": [43.66080050000001, -79.39350089999999],
+  "CH": [43.66077869999999, -79.3954499],
+  "BF": [43.66096049999999, -79.3997198],
+  "BL": [43.6649961, -79.39907579999999],
+  "VC": [43.6664436, -79.3921208],
+  "RL": [43.664486, -79.39968999999999],
+  "DA": [43.6596724, -79.400621],
+  "BT": [43.6673237, -79.3925648],
+  "CR": [43.6652263, -79.3904834],
+  "TC": [43.6651403, -79.39554199999999],
+  "ML": [43.6642354,-79.3905085],
+  "BR": [43.6663742, -79.3897593],
+  "CG": [43.66188330000001, -79.392928],
+  "BC": [43.6671086, -79.3928699],
+  "RG": [43.6639437, -79.39028069999999],
+  "LM": [43.66178439999999, -79.3977525],
+  "LA": [43.6656976, -79.3968307],
+  "IN": [43.6655322, -79.3995474],
+  "JH": [43.6677365, -79.40028819999999],
+  "UY": [43.6588089, -79.39098919999999],
+  "LI": [43.66846169999999, -79.3936494],
+  "SD": [43.6625169, -79.3974763],
+  "UP": [43.6630686,-79.3980136],
+  "GM": [43.6643906, -79.4011913],
+  "WO": [43.6674139, -79.3992146],
+  "EJ": [43.66657319999999, -79.3946105],
+  "OH": [43.6663409, -79.3887014],
+  "OA": [43.6587675, -79.39252859999999],
+  "SB": [43.6601507, -79.3999419],
+  "NB": [43.6603266, -79.4002761],
+  "RU": [43.65556369999999, -79.3889719],
+  "JP": [43.6641333, -79.389529],
+  "BN": [43.66297309999999, -79.40024489999999],
+  "WS": [43.6630539, -79.4015228],
+  "RS": [43.6600101, -79.3931363],
+  "RT": [43.66526169999999, -79.39865619999999],
+  "SM": [43.6620481, -79.39372190000002],
+  "MY": [43.6607349, -79.39661219999999],
+  "HA": [43.6599137, -79.3935427],
+  "MB": [43.6595391, -79.3934143],
+  "MC": [43.6601893, -79.3939752],
+  "SF": [43.6600106, -79.3948345],
+  "RW": [43.6631087, -79.3990019],
+  "MP": [43.6608787, -79.3984443],
+  "SK": [43.6681716, -79.3976931],
+  "HI": [43.6657106, -79.3978064],
+  "PR": [43.6663127, -79.3913932]
+}
+import requests
+import sqlite3
+
+def get_geocode(api_key, address):
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {'address': address, 'key': api_key}
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        result = response.json()
+        if result['results']:
+            location = result['results'][0]['geometry']['location']
+            return location['lat'], location['lng']
+        else:
+            return None, None
+    else:
+        return None, None
+    
+
+def get_walking_info(api_key, origin, destination):
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    params = {
+        'origins': f"{origin[0]},{origin[1]}",
+        'destinations': f"{destination[0]},{destination[1]}",
+        'mode': 'walking',
+        'key': api_key
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        result = response.json()
+        distance = result['rows'][0]['elements'][0]['distance']['text']
+        duration = result['rows'][0]['elements'][0]['duration']['text']
+        return distance, duration
+    else:
+        return None, None
+
+api_key = 'AIzaSyAIS9hioan8Vys9MR_ZfyBAzWi2E0kr4yA'
+conn = sqlite3.connect('walking_distances.db')
+cursor = conn.cursor()
+
+origin = 'HI'
+destination = 'PR'
+cursor.execute('SELECT distance, duration FROM distances WHERE origin = ? AND destination = ?', (origin, destination))
+result = cursor.fetchone()
+if result:
+    print(f"Distance: {result[0]}, Duration: {result[1]}")
+else:
+    print("No data found.")
+
+conn.close()
+
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS distances (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         origin TEXT NOT NULL,
+#         destination TEXT NOT NULL,
+#         distance TEXT,
+#         duration TEXT
+#     )
+# ''')
+
+# for origin_key, origin_value in locations.items():
+#     for destination_key, destination_value in locations.items():
+#         if origin_key != destination_key:
+#             distance, duration = get_walking_info(api_key, origin_value, destination_value)
+#             if distance and duration:
+#                 # 存储到数据库
+#                 cursor.execute('INSERT INTO distances (origin, destination, distance, duration) VALUES (?, ?, ?, ?)',
+#                                (origin_key, destination_key, distance, duration))
+#                 conn.commit()
+#                 print(f'{origin_key} to {destination_key}: {distance},{duration}')
+# conn.close()
+
+# origin_value = [43.6681716, -79.3976931]
+# destination_value = [43.6663127, -79.3913932]
+# distance, duration = get_walking_info(api_key, origin_value, destination_value)
+# print(f'wlaked: {distance},time:{}')
+
+
+
+# latlon_dic = {}
+# for code, name in building_list.items():
+#     latitude, longitude = get_geocode(api_key, name)
+#     if latitude and longitude:
+#         latlon_dic[code] = (latitude,longitude)
+#     else:
+#         latlon_dic[code] = ''
+# timetable_json = json.dumps(latlon_dic, indent=4)
+# with open("codes.json", "w") as file:
+#     file.write(timetable_json)
+    
+
+
+# latitude, longitude = get_geocode(api_key, address)
+# if latitude and longitude:
+#     print(f"The geocode for {address} is Latitude: {latitude}, Longitude: {longitude}")
+# else:
+#     print("Geocode not found for the address.")
+
+# origin = (43.6663161, -79.3990297)
+# destination = (43.6657106, -79.3978064)
+
+# walking_time = get_walking_time(api_key, origin, destination)
+# print(f"The walking time between the two points is {walking_time}")
